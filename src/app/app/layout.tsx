a0e575b5cab7ba44e2eac9c9f4580b6294ff501d
newt <ysti2019.win2020.st@gmail.com>
2024-12-15 22:51:50 +0900

    change: routng dir

--- src/app/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AppShell from "./_components/AppShell";
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <AppShell>{children}</AppShell>;
-}

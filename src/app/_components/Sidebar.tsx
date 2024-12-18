174eaa48752afd7f6c3e53f388ce14c0d7ad870c
newt <ysti2019.win2020.st@gmail.com>
2024-12-18 13:36:54 +0900

    feat: add Sidebar component

---
+++ src/app/_components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { AppShellNavbar } from "@mantine/core";
+
+const Sidebar: React.FC = () => {
+  return (
+    <AppShellNavbar bg="blue" c="white" h="full">
+      Digichat
+    </AppShellNavbar>
+  );
+};
+
+export default Sidebar;

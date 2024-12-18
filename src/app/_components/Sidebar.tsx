e8f44f152ff092511a6c6786f475a519957a0471
newt <ysti2019.win2020.st@gmail.com>
2024-12-18 17:14:26 +0900

    add: header

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,9 +1,32 @@
-import { AppShellNavbar } from "@mantine/core";
+import { ActionIcon, AppShellNavbar, Flex, Menu, Title } from "@mantine/core";
+import { IconPlus, IconUser, IconWorld } from "@tabler/icons-react";
 
 const Sidebar: React.FC = () => {
   return (
-    <AppShellNavbar bg="blue" c="white" h="full">
-      Digichat
+    <AppShellNavbar bg="blue" c="white" h="full" p="sm">
+      <Flex align="center" justify="space-between" w="full">
+        <Title order={1} size="1.5rem">
+          Digichat
+        </Title>
+        <Menu shadow="md" position="right-start">
+          <Menu.Target>
+            <ActionIcon size="lg" variant="subtle" color="white" radius="xl">
+              <IconPlus />
+            </ActionIcon>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item leftSection={<IconPlus />}>
+              新しいチャンネルを作成する
+            </Menu.Item>
+            <Menu.Item leftSection={<IconWorld />}>
+              チャンネルを閲覧する
+            </Menu.Item>
+            <Menu.Item leftSection={<IconUser />}>
+              ダイレクトメッセージを開く
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </Flex>
     </AppShellNavbar>
   );
 };

98985086ac32b685208666f3cd69971b85fa381f
newt <ysti2019.win2020.st@gmail.com>
2024-12-23 14:29:54 +0900

    add: use Popover component on Emoji Picker

--- src/components/EmojiPickerControl.tsx
+++ src/components/EmojiPickerControl.tsx
@@ -1,41 +1,34 @@
 import { EmojiData } from "#/types/emoji";
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
+import { Popover } from "@mantine/core";
 import {
   RichTextEditorControl,
   useRichTextEditorContext,
 } from "@mantine/tiptap";
 import { IconMoodSmile } from "@tabler/icons-react";
-import { useState } from "react";
+
+import styles from "./EmojiPicker.module.css";
 
 const EmojiPickerControl = () => {
   const { editor } = useRichTextEditorContext();
-  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   return (
-    <>
-      <RichTextEditorControl
-        onClick={() => setShowEmojiPicker(!showEmojiPicker)}
-        title="Insert emoji"
-        aria-label="Insert emoji"
-      >
-        <IconMoodSmile stroke={1.5} size="1rem" />
-      </RichTextEditorControl>
-      {showEmojiPicker && (
+    <Popover width={200} position="bottom">
+      <Popover.Target>
+        <RichTextEditorControl title="Insert emoji" aria-label="Insert emoji">
+          <IconMoodSmile stroke={1.5} size="1rem" />
+        </RichTextEditorControl>
+      </Popover.Target>
+      <Popover.Dropdown className={styles.PopoverDropdown}>
         <Picker
-          style={{
-            position: "absolute",
-            bottom: "2rem",
-            right: "2rem",
-            zIndex: 1000,
-          }}
           data={data}
           onEmojiSelect={(emoji: EmojiData) => {
             editor?.chain().focus().insertContent(emoji.native).run();
           }}
         />
-      )}
-    </>
+      </Popover.Dropdown>
+    </Popover>
   );
 };
 

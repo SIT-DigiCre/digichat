f8333e11a8bc08ae222157b9e4722484765aff16
newt <ysti2019.win2020.st@gmail.com>
2025-01-07 14:51:55 +0900

    refactor: update variable name

--- src/components/EmojiPickerControl.tsx
+++ src/components/EmojiPickerControl.tsx
@@ -16,69 +16,62 @@ import { EmojiData } from "#/types/emoji";
 
 const EmojiPickerControl = () => {
   const { editor } = useRichTextEditorContext();
-  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   const [drawerOpened, setDrawerOpened] = useState(false);
 
   const handleEmojiSelect = (emoji: EmojiData) => {
     editor?.chain().focus().insertContent(emoji.native).run();
-    if (isMobile) {
+    if (isDesktop) {
       setDrawerOpened(false);
     }
   };
 
-  return (
-    <>
-      {isMobile ? (
-        <>
-          <RichTextEditorControl
-            title="Insert emoji"
-            aria-label="Insert emoji"
-            onClick={() => setDrawerOpened(true)}
-          >
+  if (isDesktop) {
+    return (
+      <>
+        <RichTextEditorControl
+          title="Insert emoji"
+          aria-label="Insert emoji"
+          onClick={() => setDrawerOpened(true)}
+        >
+          <IconMoodSmile stroke={1.5} size="1rem" />
+        </RichTextEditorControl>
+        <Drawer
+          classNames={{
+            content: styles.DrawerContent,
+            body: styles.DrawerBody,
+          }}
+          withCloseButton={false}
+          position="bottom"
+          opened={drawerOpened}
+          onClose={() => setDrawerOpened(false)}
+        >
+          <Picker
+            style={{
+              width: "100%",
+            }}
+            dynamicWidth
+            theme="light"
+            data={data}
+            onEmojiSelect={handleEmojiSelect}
+          />
+        </Drawer>
+      </>
+    );
+  } else {
+    return (
+      <Popover position="bottom">
+        <Popover.Target>
+          <RichTextEditorControl title="Insert emoji" aria-label="Insert emoji">
             <IconMoodSmile stroke={1.5} size="1rem" />
           </RichTextEditorControl>
-          <Drawer
-            classNames={{
-              content: styles.DrawerContent,
-              body: styles.DrawerBody,
-            }}
-            withCloseButton={false}
-            position="bottom"
-            opened={drawerOpened}
-            onClose={() => setDrawerOpened(false)}
-          >
-            <Picker
-              style={{
-                width: "100%",
-              }}
-              dynamicWidth
-              theme="light"
-              data={data}
-              onEmojiSelect={handleEmojiSelect}
-            />
-          </Drawer>
-        </>
-      ) : (
-        <Popover position="bottom">
-          <Popover.Target>
-            <RichTextEditorControl
-              title="Insert emoji"
-              aria-label="Insert emoji"
-            >
-              <IconMoodSmile stroke={1.5} size="1rem" />
-            </RichTextEditorControl>
-          </Popover.Target>
-          <Popover.Dropdown className={styles.PopoverDropdown}>
-            <Picker
-              theme="light"
-              data={data}
-              onEmojiSelect={handleEmojiSelect}
-            />
-          </Popover.Dropdown>
-        </Popover>
-      )}
-    </>
-  );
+        </Popover.Target>
+        <Popover.Dropdown className={styles.PopoverDropdown}>
+          <Picker theme="light" data={data} onEmojiSelect={handleEmojiSelect} />
+        </Popover.Dropdown>
+      </Popover>
+    );
+  }
 };
 
 export default EmojiPickerControl;

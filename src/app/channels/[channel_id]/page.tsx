8beaf1e90373c6d81c289c0147e486704b1eeede
newt <ysti2019.win2020.st@gmail.com>
2025-01-28 16:51:25 +0900

    feat: show messages per channel_id

--- src/app/channels/[channel_id]/page.tsx
+++ src/app/channels/[channel_id]/page.tsx
@@ -1,5 +1,41 @@
-async function ChannelIDPage() {
-  return <div>digichat</div>;
+import Message from "#/components/Message";
+import { prisma } from "#/libs/prisma";
+
+type ChannelIDPageProps = {
+  params: Promise<{ channel_id: string }>;
+};
+
+async function ChannelIDPage({ params }: ChannelIDPageProps) {
+  const { channel_id } = await params;
+  const messages = await prisma.message.findMany({
+    where: {
+      id: channel_id,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  return (
+    <div>
+      {messages.map((message) => (
+        <Message
+          key={message.id}
+          message={{
+            id: message.id,
+            channel_id: message.channelId,
+            created_at: message.createdAt,
+            content: message.content,
+          }}
+          user={{
+            id: message.user.id,
+            name: message.user.name,
+            icon_url: message.user.iconUrl,
+          }}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ChannelIDPage;

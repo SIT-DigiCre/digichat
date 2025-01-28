524e055a0a25b0b978651f9081c49eef1cb06398
newt <ysti2019.win2020.st@gmail.com>
2025-01-28 16:23:52 +0900

    feat: get messages with prisma

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -1,22 +1,46 @@
 import { Stack, Title } from "@mantine/core";
+import { PrismaClient } from "@prisma/client";
 
 import LinkCard from "#/components/LinkCard";
 import Message from "#/components/Message";
 
-const LabPage = () => {
+export async function LabPage() {
+  console.log(process.env.POSTGRES_URL);
+  const client = new PrismaClient();
+  const messages = await client.message.findMany({
+    include: {
+      user: true,
+    },
+  });
   return (
     <div>
+      {messages.map((message) => (
+        <Message
+          key={message.id}
+          message={{
+            id: message.id,
+            channel_id: message.channelId,
+            created_at: message.createdAt,
+            content: message.content,
+          }}
+          user={{
+            id: message.user.id,
+            name: message.user.name,
+            icon_url: message.user.iconUrl,
+          }}
+        />
+      ))}
       <Message
         message={{
           channel_id: "1",
-          message_id: "1",
-          created_at: "2021-10-01T00:00:00Z",
+          id: "1",
+          created_at: new Date(),
           content: "Hello, World!",
         }}
         user={{
           id: "1",
           name: "John Doe",
-          avatar_url: "https://example.com/avatar.jpg",
+          icon_url: "https://example.com/avatar.jpg",
         }}
       />
       <Title order={2}>コンポーネントのプレビュー用</Title>
@@ -28,6 +52,6 @@ const LabPage = () => {
       </Stack>
     </div>
   );
-};
+}
 
 export default LabPage;
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,30 +1,33 @@
 import { Avatar, Box, Flex, Stack, Text, Title } from "@mantine/core";
+import dayjs from "dayjs";
 
 import styles from "./Message.module.css";
 
 export type MessageProps = {
   message: {
+    id: string;
     channel_id: string;
-    message_id: string;
-    created_at: string;
+    created_at: Date;
     content: string;
   };
   user: {
     id: string;
     name: string;
-    avatar_url: string;
+    icon_url: string | null;
   };
 };
 
 const Message: React.FC<MessageProps> = (props) => {
   return (
     <Flex className={styles.message} gap="sm">
-      <Avatar src={props.user.avatar_url} alt={props.user.name} radius="xl" />
-      <Stack gap="xs">
-        <Flex align="center" gap="sm">
-          <Title order={4}>{props.user.name}</Title>
+      <Avatar src={props.user.icon_url} alt={props.user.name} radius="xl" />
+      <Stack gap={0}>
+        <Flex align="center" gap="xs">
+          <Title order={4} size="sm">
+            {props.user.name}
+          </Title>
           <Text size="xs" c="dimmed">
-            {props.message.created_at}
+            {dayjs(props.message.created_at).format("HH:mm")}
           </Text>
         </Flex>
         <Box>{props.message.content}</Box>

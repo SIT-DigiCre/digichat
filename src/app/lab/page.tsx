8bef554e765a26359260fad1a5e7037acc52b155
newt <ysti2019.win2020.st@gmail.com>
2025-01-29 13:44:30 +0900

    remove: Message components from lab page

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -1,47 +1,10 @@
 import { Stack, Title } from "@mantine/core";
-import { PrismaClient } from "@prisma/client";
 
 import LinkCard from "#/components/LinkCard";
-import Message from "#/components/Message";
 
 async function LabPage() {
-  const client = new PrismaClient();
-  const messages = await client.message.findMany({
-    include: {
-      user: true,
-    },
-  });
   return (
     <div>
-      {messages.map((message) => (
-        <Message
-          key={message.id}
-          message={{
-            id: message.id,
-            channel_id: message.channelId,
-            created_at: message.createdAt,
-            content: message.content,
-          }}
-          user={{
-            id: message.user.id,
-            name: message.user.name,
-            icon_url: message.user.iconUrl,
-          }}
-        />
-      ))}
-      <Message
-        message={{
-          channel_id: "1",
-          id: "1",
-          created_at: new Date(),
-          content: "Hello, World!",
-        }}
-        user={{
-          id: "1",
-          name: "John Doe",
-          icon_url: "https://example.com/avatar.jpg",
-        }}
-      />
       <Title order={2}>コンポーネントのプレビュー用</Title>
       <Stack gap="md">
         <LinkCard href="https://github.com/SIT-DigiCre/digichat" />
